97c76887a1755a5d6f667b8a4a92cbf8a340422c
Keylane <keylanessilva07@gmail.com>
2021-06-30 00:31:25 -0300

    add validação do form

--- routes/admin.js
+++ routes/admin.js
@@ -20,20 +20,29 @@ router.post('/categorias/nova', (req, res) =>{
     // Validação do formulario
     var erros = []
 
-    if(!req.body.nome && !req.body.slug){
-            
+    if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+        erros.push("Nome inválido")
     }
-    
-    const novaCategoria = {
-        nome: req.body.nome,
-        slug: req.body.slug
+    if((req.body.nome).length < 2){
+        erros.push("Nome muito curto")
     }
+    if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null){
+        erros.push("Slug inválido")
+    }
+    if(erros.length > 0){
 
-    new Categoria(novaCategoria).save().then(() =>{
-        console.log("Categoria salva com sucesso")
-    }).catch((err) => {
-        console.log("Eroo ao salvar categoria: "+err)
-    })
+    }else{ 
+        const novaCategoria = {
+            nome: req.body.nome,
+            slug: req.body.slug
+        }
+        
+        new Categoria(novaCategoria).save().then(() =>{
+            console.log("Categoria salva com sucesso")
+        }).catch((err) => {
+            console.log("Eroo ao salvar categoria: "+err)
+        })
+    }
 })
 
 module.exports = router
