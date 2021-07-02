5e9c52d62cc67fe92c0a54b3b8df1e5730a95e83
Keylane <keylanessilva07@gmail.com>
2021-07-02 11:35:28 -0300

    bug corrigido

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,7 @@ router.get('/', (req,res) => {
 })
 
 router.get('/categorias', (req,res) => {
-    Categoria.find().then((categorias) => {
+    Categoria.find().sort({data: 'desc'}).lean().then((categorias) => {
         res.render("admin/categorias", {categorias: categorias})
     }).catch((err) =>{
         req.flash("error_msg", "Houve um erro ao listar as categorias")
