37afa34ae38ed21e34ad29a2d79a172cef941e34
Keylane <keylanessilva07@gmail.com>
2021-07-09 12:00:42 -0300

    criando p categoria

--- app.js
+++ app.js
@@ -8,8 +8,11 @@
     const path = require('path')
     const session = require('express-session')
     const flash = require('connect-flash')
-    require("./models/Postagem")
-    const Postagem = mongoose.model("postagens")
+    require('./models/Postagem')
+    require('./models/Categoria')
+    const Postagem = mongoose.model('postagens')
+    const Categoria = mongoose.model('categorias')
+
 
 // Configurações
     // Sessão
@@ -54,6 +57,51 @@
         })
     })
 
+    app.get('/postagem/:slug', (req, res) => {
+        Postagem.findOne({slug: req.params.slug}).lean().then((postagem) => {
+            if(postagem){
+                res.render("postagem/index", {postagem: postagem})
+            }else{
+                req.flash("error_msg", "Está postagem não existe!")
+                res.redirect('/')
+            }
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro interno!")
+                res.redirect('/')
+        })
+    })
+
+    // Página de categorias
+    app.get('/categorias', (req, res) => {
+        Categoria.find().lean().then((categorias) => {
+            res.render('categorias/index', {categorias: categorias})
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro interno ao listar as categorias")
+            res.redirect('/')
+        })
+    })
+
+    // Linkando as categorias com as postagens
+    app.get('/categorias/:slug', (req, res) => {
+        Categoria.findOne({slug: req.params.slug}).lean().then((categoria) => {
+            if(categoria){
+                Postagem.find({categoria: categoria._id}).then((postagens) => {
+                    res.render('categorias/postagens', {postagens: postagens, categoria: categoria})
+                }).catch((err) => {
+                    req.flash("error_msg", "Houve um erro ao listar os posts")
+                    res.redirect('/')
+                })
+            }else{
+                req.flash("error_msg", "Está categoria não existe!")
+                res.redirect('/')
+            }
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro carregar a pagina de posts!")
+            res.redirect('/')
+        })
+    })
+
+    //Página de erro
     app.get('/404', (req, res) => {
         res.send('Erro 404')
     })
